a0db417bc8f14baa77a92492029e35d6f4c3f979
Debjeet Mukherjee <91367172+CoderDebjeet@users.noreply.github.com>
2022-12-20 16:57:39 +0530

    Add files via upload

---
+++ Attendance System using Face Recognition/main.py
@@ -0,0 +1,77 @@
+import face_recognition
+import cv2
+import numpy as np
+import csv
+import os
+from datetime import datetime
+video_capture=cv2.VideoCapture(0)
+
+jobs_image = face_recognition.load_image_file("images/jobs.jpg")
+jobs_encoding= face_recognition.face_encodings(jobs_image)[0]
+
+ratan_tata_image = face_recognition.load_image_file("images/tata.jpg")
+ratan_tata_encoding= face_recognition.face_encodings(ratan_tata_image)[0]
+
+mona_image = face_recognition.load_image_file("images/mona.jpg")
+mona_encoding= face_recognition.face_encodings(mona_image)[0]
+
+tesla_image = face_recognition.load_image_file("images/tesla.jpeg")
+tesla_encoding= face_recognition.face_encodings(tesla_image)[0]
+
+known_face_encoding=[
+    jobs_encoding,
+    ratan_tata_encoding,
+    mona_encoding,
+    tesla_encoding
+    ]
+
+known_faces_names=["jobs","tata","mona","tesla"]
+
+students = known_faces_names.copy()
+
+face_locations=[] 
+face_encodings=[] 
+face_names=[]
+s=True
+
+now=datetime.now()
+current_date=now.strftime("%Y-%m-%d")
+
+f=open(current_date+'.csv','w+',newline='')
+lnwriter=csv.writer(f)
+
+while True:
+     _,frame=video_capture.read() #extracting video data
+     small_frame=cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+     rgb_small_frame=small_frame[:,:,::-1]
+     if s:
+         face_locations=face_recognition.face_locations(rgb_small_frame) #store data from the coming frames
+         face_encodings=face_recognition.face_encodings(rgb_small_frame,face_locations)
+         face_names=[]
+         for face_encoding in face_encodings:
+             matches=face_recognition.compare_faces(known_face_encoding,face_encoding)
+             name=""
+             face_distance=face_recognition.face_distance(known_face_encoding,face_encoding)
+             best_match_index=np.argmin(face_distance) 
+             if matches[best_match_index]:
+                 name=known_faces_names[best_match_index]
+
+             face_names.append(name)
+             if name in known_faces_names:
+                if name in students:
+                    students.remove(name)
+                    print(students)
+                    current_time=now.strftime("%H-%M-%S")
+                    lnwriter.writerow([name,current_time])
+     cv2.imshow("attendance system",frame)
+     if cv2.waitKey(1) & 0xFF == ord('q'):   # exit condition by pressing q button
+          break
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
+            
+                 
+                 
+    
+
+
